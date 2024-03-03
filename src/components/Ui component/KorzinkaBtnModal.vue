607f205265faefdd2cd76b108f8cd1e19e742a15
NODIRBEKIBRAGIMOV <nodirbekibragimov2005@gmail.com>
2024-03-03 17:20:35 +0500

    .

--- src/components/Ui component/KorzinkaBtnModal.vue
+++ src/components/Ui component/KorzinkaBtnModal.vue
@@ -1,27 +1,49 @@
 <template>
     <div>
-        <button type="button" style="border: none; background: none; padding: 0;" data-bs-toggle="offcanvasw"
-            data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class='bx bx-basket'><span>{{ counter
-            }}</span></i></button>
-        <div class="offcanvasw offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <button type="button" style="border: none; background: red; padding: 0;" data-bs-toggle="offcanvasw"
+            data-bs-target="#offcanvaswRight" aria-controls="offcanvaswRight"><i class='bx bx-basket'><span>{{ counter
+                    }}</span></i></button>
+        <div class="offcanvasw offcanvasw-end" tabindex="-1" id="offcanvaswRight" aria-labelledby="offcanvaswRightLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasRightLabel">Savatcha ({{ counter }})</h5>
+                <h5 class="offcanvasw-title" id="offcanvaswRightLabel">Savatcha ({{ counter }})</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvasw" aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body">
+            <div class="offcanvasw-body">
                 <button v-on:click="min">-</button>
                 <button v-on:click="ondec">+</button>
             </div>
         </div>
     </div>
 </template>
+
 <script>
 export default {
-    
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        ondec() {
+            if (this.counter < 10) {
+                this.counter += 1
+            } else {
+                alert('error')
+            }
+        },
+        min() {
+            if (this.counter > 0) {
+                this.counter -= 1
+            } else {
+                this.counter = 0
+            }
+        }
+    }
 }
 </script>
+
 <style scoped>
-    button i {
+button i {
     color: #7777779c;
     font-size: 22px;
     margin: 0 5px;
