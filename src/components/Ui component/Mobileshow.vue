e856c134f7088f0a2b4ecbe4a293cee0128576d3
NODIRBEKIBRAGIMOV <nodirbekibragimov2005@gmail.com>
2024-03-03 18:12:51 +0500

    .

--- src/components/Ui component/Mobileshow.vue
+++ src/components/Ui component/Mobileshow.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="mobile-show">
         <RouterLink to="/"><span><i class='bx bx-home'></i></span></RouterLink>
-        <span><i class='bx bx-user'></i>
+        <span>
+            <!-- <i class='bx bx-user'></i> -->
             <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Enable body scrolling</button>
 
@@ -10,6 +11,27 @@
                 <div class="offcanvas-header">
                     <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
+                </div>
+                <div class="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
+                </div>
+                <div class="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
+                </div>
+                <div class="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
+                </div>
+                <div class="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
+                </div>
+                <div class="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
+                </div>
+                <div class="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
                 </div>
                 <div class="offcanvas-body">
                     <p>Try scrolling the rest of the page to see this option in action.</p>
