08b8a5958edf46615a510989c344519868117d69
NODIRBEKIBRAGIMOV <nodirbekibragimov2005@gmail.com>
2024-03-03 17:16:17 +0500

    btn

--- src/components/Ui component/KorzinkaBtnModal.vue
+++ src/components/Ui component/KorzinkaBtnModal.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
-        <h1><button type="button" style="border: none; background: none; padding: 0;" data-bs-toggle="offcanvas"
+        <button type="button" style="border: none; background: none; padding: 0;" data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class='bx bx-basket'><span>{{ counter
-            }}</span></i></button></h1>
-        
+            }}</span></i></button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">Savatcha ({{ counter }})</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <button v-on:click="min">-</button>
+                <button v-on:click="ondec">+</button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
