5efc7eebae305b8a04f6905b4aba3e1fb82e6782
NODIRBEKIBRAGIMOV <nodirbekibragimov2005@gmail.com>
2024-03-03 17:08:27 +0500

    .

--- src/components/Ui component/Mobileshow.vue
+++ src/components/Ui component/Mobileshow.vue
@@ -7,7 +7,7 @@
     </div>
 </template>
 <script>
-import KorzinkaBtnModal from './KorzinkaBtnModal'
+import KorzinkaBtnModal from './KorzinkaBtnModal.vue'
 export default {
     components: {KorzinkaBtnModal}
 }
